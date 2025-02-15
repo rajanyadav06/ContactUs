a8d4a3a772c60718ce9bfb1f51e4c6d284b0ce1d
Rajan Yadav <rajanwebstudio@gmail.com>
2025-02-15 11:03:34 +0530

    Link Add

--- src/Components/MapComponent.jsx
+++ src/Components/MapComponent.jsx
@@ -2,10 +2,10 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 const MapComponent = () => {
-    const position = [28.957889, 77.6330404]; // लखनऊ का लोकेशन
+    const position = [28.5893577,77.3120101]; // Loaction 91Springboard Sector 1, Noida
 
     return (
-        <div className="w-full flex justify-center items-center">
+        <div className="w-full flex justify-center items-center">    
             <MapContainer
                 center={position}
                 zoom={13}
@@ -15,8 +15,8 @@ const MapComponent = () => {
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     
                 />
-                <Marker position={position}>
-                    <Popup>यहाँ आपका लोकेशन है!</Popup>
+                <Marker  position={position}>
+                    <Popup>91Springboard Sector 1, Noida</Popup>
                 </Marker>
             </MapContainer>
         </div>
